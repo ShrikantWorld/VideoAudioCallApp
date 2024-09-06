38ed7453ba82818d5f569b6eec86adca3d01fcda
ShrikantWorld <shrikantkgacche@gmail.com>
2024-09-07 01:37:55 +0530

    update realtime server with socketjs
    socketjs with realtime server updated

--- serverbackup.js
+++ serverbackup.js
@@ -1,30 +1,23 @@
 const express = require('express');
-const path = require('path');
-const { createServer } = require('http');
+const http = require('http');
 const socketIO = require('socket.io');
 
-// Initialize Express
 const app = express();
-
-// Serve static files from a 'static' directory
-app.use('/', express.static(path.join(__dirname, 'static')));
-
-// Create HTTP server
-const httpServer = createServer(app);
-
-// Initialize Socket.IO with the created HTTP server
-const io = socketIO(httpServer);
+const server = http.createServer(app);
+const io = socketIO(server);
 
 let connectedUsers = {};
 
-// Handle Socket.IO connections
+// Serve a simple message at the root path
+app.get('/', (req, res) => {
+  res.send('Signaling server is running.');
+});
+
 io.on('connection', (socket) => {
   connectedUsers[socket.id] = socket;
 
-  // Emit updated user list
   io.emit('updateUserList', Object.keys(connectedUsers));
 
-  // Handle call initiation
   socket.on('startCall', (data) => {
     const { to } = data;
     if (connectedUsers[to]) {
@@ -32,7 +25,6 @@ io.on('connection', (socket) => {
     }
   });
 
-  // Handle signal exchange
   socket.on('signal', (data) => {
     const { to, ...signalData } = data;
     if (connectedUsers[to]) {
@@ -40,16 +32,12 @@ io.on('connection', (socket) => {
     }
   });
 
-  // Handle disconnection
   socket.on('disconnect', () => {
     delete connectedUsers[socket.id];
     io.emit('updateUserList', Object.keys(connectedUsers));
   });
 });
 
-// Set the server to listen on the specified port or default to 3500
-let port = process.env.PORT || 3500;
-
-httpServer.listen(port, () => {
-  console.log("Server started on port", port);
+server.listen(3000, () => {
+  console.log('Signaling server is running on port 3000');
 });
